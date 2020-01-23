09fb512d1181c04e8e60cd38044c4bacb52fde1c
carloserlemus <carlosriveralemus@gmail.com>
2020-01-22 18:07:06 -0800

    more exercises!

---
+++ js/index20.js
@@ -0,0 +1,7 @@
+let day = 22;
+let month = 1;
+let year = 2020;
+
+let currentDate = `${day}/${month}/${year}`;
+
+console.log(currentDate);
---
+++ js/index21.js
@@ -0,0 +1,13 @@
+let age = 5000;
+let phone = 1800999999;
+let streetName = "Fake St.";
+let streetNumber = 12345;
+
+let ageMsg = `I'm ${age} years old.`;
+let phoneMsg = `My phone number is ${phone}`;
+let address = `I live on ${streetNumber} ${streetName}`;
+
+console.log(ageMsg);
+console.log(phoneMsg);
+console.log(address);
+
