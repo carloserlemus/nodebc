1d09f4502e315abfad3e59a8b1da76da892bcb68
carloserlemus <carlosriveralemus@gmail.com>
2020-01-27 20:09:43 -0800

    Added index69.js

---
+++ js/index69.js
@@ -0,0 +1,14 @@
+/*
+Use while structure to sum numbers between 0 and 1000
+Show the partial result as output too
+-----
+notes
+-----
+    add numbers between 0 and 1000. Show the current and the increment.
+*/
+
+let number = 0;
+
+while (number <= 100){
+    console.log('The sum of the current number %o and 1 is equal to %o', number, ++number)
+}
