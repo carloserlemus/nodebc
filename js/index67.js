ebea0fbf5a0973495986cd485a6827d87394c0fb
carloserlemus <carlosriveralemus@gmail.com>
2020-01-27 19:03:17 -0800

    Added two exercises by accident because I'm dumb.

---
+++ js/index67.js
@@ -0,0 +1,8 @@
+// Use while structure to show numbers between 100 and 0 as output
+
+let number = 0;
+
+while (number <= 100){
+    console.log('The number is: %o', number)
+    number += 5; // 5 because all 100 is too much.
+}
---
+++ js/index68.js
@@ -0,0 +1,8 @@
+// Use while structure to only show even numbers between 0 and 100 as output
+
+let number = 0;
+
+while (number <= 100){
+    console.log('The number is: %o', number)
+    number += 2
+}
