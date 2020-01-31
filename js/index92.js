70a2a248a38d9602af8eb6061a94500cf6acd40e
carloserlemus <carlosriveralemus@gmail.com>
2020-01-30 20:43:19 -0800

    added 92

---
+++ js/index92.js
@@ -0,0 +1,8 @@
+/*
+Copy and paste the code from exercise 82
+Refactor the code to use for instead of do/while
+*/
+for (number = 0, star = '*'; number < 15; number++){
+    console.log(star)
+    star += '*'
+}
