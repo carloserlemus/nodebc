a5ce5fa92d8a3c722d88039108f5ba46f9035f9a
carloserlemus <carlosriveralemus@gmail.com>
2020-01-30 20:42:47 -0800

    added 88

---
+++ js/index88.js
@@ -0,0 +1,8 @@
+/*
+Copy and paste the code from exercise 78
+Refactor the code to use for instead of do/while
+*/
+
+for (let number = 0; number <= 100; number += 2){
+    console.log('The number is: %o', number)
+}
