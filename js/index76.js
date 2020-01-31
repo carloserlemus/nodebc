dc062849f6e65df27145fbe1f43b6a3997ec75a0
carloserlemus <carlosriveralemus@gmail.com>
2020-01-30 20:38:54 -0800

    added 76

---
+++ js/index76.js
@@ -0,0 +1,18 @@
+/*
+Create a new index76.js file
+Copy and paste the code from exercise 66
+Refactor the code to use do/while instead of while
+*/
+let number = 0;
+/*
+while (number <= 10){
+    console.log('The number is %o', number)
+    number++;
+}
+*/
+
+do {
+    console.log('The number is %o', number)
+    number++    
+} while (number <=10)
+
