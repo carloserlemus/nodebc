96c22a419d793eea82956ea24bd935017330bcae
carloserlemus <carlosriveralemus@gmail.com>
2020-01-30 20:42:55 -0800

    added 89

---
+++ js/index89.js
@@ -0,0 +1,7 @@
+/* 
+Copy and paste the code from exercise 79
+Refactor the code to use for instead of do/while
+*/
+for (let number = 0; number <=100; number++){
+    console.log('The sum of the current number %o and 1 is equal to %o', number, ++number)
+}
