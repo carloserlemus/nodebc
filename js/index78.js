c48a8b607f9dfc276d34442f1d1cccafa1b2da6d
carloserlemus <carlosriveralemus@gmail.com>
2020-01-30 20:39:19 -0800

    added 78

---
+++ js/index78.js
@@ -0,0 +1,10 @@
+/*
+Copy and paste the code from exercise 68
+Refactor the code to use do/while instead of while
+*/
+let number = 0;
+
+do {
+    console.log('The number is: %o', number)
+    number += 2
+} while (number <= 100)
