6ff91a652b7ed0ec6c2803df2b44a474df3614e0
carloserlemus <carlosriveralemus@gmail.com>
2020-01-30 20:43:12 -0800

    added 91

---
+++ js/index91.js
@@ -0,0 +1,8 @@
+/*
+Copy and paste the code from exercise 81
+Refactor the code to use for instead of do/while
+*/
+for (multi2 = 0; multi2 <= 9; multi2++){
+    let multi1 = 9
+    console.log(multi1 + ' times ' + (multi2) + ' equals ' + (multi1 * (multi2)))
+}
