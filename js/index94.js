0f3b8a33680cb09727fa3dc8c0d97c5ea2634513
carloserlemus <carlosriveralemus@gmail.com>
2020-01-30 20:44:10 -0800

    added 94

---
+++ js/index94.js
@@ -0,0 +1,8 @@
+/*
+Copy and paste the code from exercise 84
+Refactor the code to use for instead of do/while
+*/
+for (loopLimiter = 0, ascii = '*'; loopLimiter <= 7; loopLimiter++){
+    console.log(ascii)
+    ascii +='**'
+}
