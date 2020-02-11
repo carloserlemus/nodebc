cae773ac7dc52692fd31cf07f6c9d2c4a181e7d5
carloserlemus <carlosriveralemus@gmail.com>
2020-02-11 01:39:09 -0800

    Add 103

---
+++ js/index103.js
@@ -0,0 +1,17 @@
+/*
+Define a evenNumbers variable and assign a function as value
+When executed the evenNumbers function will print even numbers between 0 and 100 as output
+Execute this function 5 times
+*/
+// Changed to 10 because I want to see it run in my terminal.
+let evenNumbers = function(){
+    for (i = 0; i <= 10; i +=2){
+        console.log(i)
+    }
+}
+
+evenNumbers();
+evenNumbers();
+evenNumbers();
+evenNumbers();
+evenNumbers();
