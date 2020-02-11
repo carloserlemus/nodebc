a437f7d5a026e5b4dffeed162e520fd0eece4f15
carloserlemus <carlosriveralemus@gmail.com>
2020-02-11 01:36:32 -0800

    Added 100

---
+++ js/index100.js
@@ -0,0 +1,10 @@
+// Define a function that will show even numbers from 0 to 100 as output
+
+function evenSteven(){ // Defined a function! Used a loop to output even numbers.
+    for (i = 0; i <= 100; i +=2){
+        console.log(i)
+    }
+}
+
+evenSteven();
+
