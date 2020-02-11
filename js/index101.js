bbeec61bbeef00f998fd4739de61b17c52dbe284
carloserlemus <carlosriveralemus@gmail.com>
2020-02-11 01:38:21 -0800

    added 101

---
+++ js/index101.js
@@ -0,0 +1,21 @@
+/*
+Define a function named showUser that will output the following data:
+Your name
+Your age
+Your phone number
+Your street
+Your postal code
+If you're married or not (boolean)
+*/
+
+function showUser(){
+    let name = 'Carlos'; let age = 35; let phone = 18009999; let street = '1444 Fake Street'; postCode = 'N0N3'; married = null
+    console.log('Name: ' + name);
+    console.log('Age: ' + age);
+    console.log('Phone Number: ' + phone);
+    console.log('Address : ' + street);
+    console.log('Postal Code: ' + postCode);
+    console.log('Marital Status: ' + married);
+}
+
+showUser();
