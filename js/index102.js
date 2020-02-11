04b4bc72902a796d0b71209a79b624a0513d835b
carloserlemus <carlosriveralemus@gmail.com>
2020-02-11 01:38:59 -0800

    Add 102

---
+++ js/index102.js
@@ -0,0 +1,17 @@
+/*
+Define a showName variable and assign a function as value
+When the showName function get executed it will print your name with the following output format:
+===========
+= Nicolás =
+===========
+Call the showName at least 2 times
+*/
+
+let showName = function(){
+    console.log('==========');
+    console.log('= Carlos =');
+    console.log('==========');
+}
+
+showName();
+showName();
