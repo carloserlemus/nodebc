8798ae4efa55d5cceccacb5b4346b1481e8871b3
carloserlemus <carlosriveralemus@gmail.com>
2020-03-03 19:54:37 -0800

    added exercise 11 and some notes...

---
+++ nodeJS/exercise11/index.js
@@ -0,0 +1,27 @@
+/*
+Create a Express web server using port 3000
+Create the following routes:
+GET '/': send the index.html file
+GET '/products': send the products.html file
+*/
+
+const express = require('express');
+const app = express();
+const port = 3000;
+const path = require('path');
+
+app.get('/', (request, response) => {
+    response.sendFile(path.join(__dirname, 'index.html'))
+})
+
+app.get('/products', (request, response) => {
+    response.sendFile(path.join(__dirname, 'products.html'))
+})
+
+app.listen(port, (err) => {
+    if (err){
+        console.log('Error!')
+    } else {
+        console.log('Smooth sailing.')
+    }
+})
