d5a3a04cd30642711e873789ad50d6a8d4a8e282
carloserlemus <carlosriveralemus@gmail.com>
2020-03-06 11:54:50 -0800

    added ex 16 of node js

---
+++ nodeJS/exercise16/index.js
@@ -0,0 +1,9 @@
+/*
+Create a Express web server using port 3000
+Configure express static folder to use the public folder
+Create an assets alias
+*/
+
+const express = require('express');
+const app = express()
+const port = 3000
