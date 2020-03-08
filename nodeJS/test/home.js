9ac752a7124716d915ecdb1d4f42683a0de80105
carloserlemus <carlosriveralemus@gmail.com>
2020-03-08 13:41:27 -0700

    Reorganized folder structure

--- nodeJS/test/home.js
+++
@@ -1,9 +0,0 @@
-// Create the express router to handle our home requests
-var express = require('express');
-var router = express.Router();
-
-router.get('/', function(req, res) {
-  res.send('Welcome to our Site!');
-});
-
-module.exports = router;
--- nodeJS/test/index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const app = express();
-
-// Routers
-const home = require('./home.js');
-const products = require('./products.js');
-
-app.use('/', home);
-app.use('/products', products);
