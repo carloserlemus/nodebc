d0b6bb457021487ccbf2ea5e0015ae573fcd02ae
carloserlemus <carlosriveralemus@gmail.com>
2020-03-12 23:13:00 -0700

    Adding MongoDB Stuff

--- mongoDB/index.js
+++ mongoDB/index.js
@@ -1,21 +1,16 @@
-// Require Mongo from MongoDB module.
 const mongoClient = require('mongodb').MongoClient;
-const express = require('express');
-const app = express();
-
-// MongoDB url to connect to...
 const url = 'mongodb://localhost:27017';
+const express = require('express');
+const app = express()
 
 app.get('/', (req, res) => {
-	MongoClient.connect(url, function(err, client) {
+	mongoClient.connect(url, function(error, client) {
 		const db = client.db('comics');
 		const collection = db.collection('superheroes');
-
+	
 		collection.find({}).toArray((error, documents) => {
 			client.close();
 			res.render('index', { documents: documents });
 		});
 	});
-});
-
-
+})
